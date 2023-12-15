9133c409ba0c6b1e74ebafd265c0d8c7942b3863
Valentyn13 <valentyn13git@gmail.com>
2023-12-15 19:33:25 +0200

    user controller for crud operations created

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -5,3 +5,32 @@ export const signUpController = (req, res, next) => {
     res.locals.queryResult = newUser
     next()
 }
+
+
+export const getAllUsersController = (req, res, next) => {
+    const users = userService.getAllUsers()
+    res.locals.queryResult = users
+    next()
+}
+
+export const updateUserController = (req,res,next) => {
+    const id = req.params.id
+    const data = req.body
+    const patchedUser = userService.updateUser(id,data)
+    res.locals.queryResult = patchedUser
+    next()
+}
+
+export const deletedUserController = (req, res, next) => {
+    const id = req.params.id
+    const deletedUser = userService.deleteUser(id)
+    res.locals.queryResult = deletedUser
+    next()
+}
+
+export const getUserById = (req, res, next) => {
+    const id = req.params
+    const user = userService.search(id)
+    res.locals.queryResult = user
+    next()
+}
