cf1066532d4a33b9d98aa31d46aa2dd2ce95d9dd
Valentyn13 <valentyn13git@gmail.com>
2023-12-15 18:39:45 +0200

    createFighterController and createFighterValid functions created

--- controllers/fighters.controller.js
+++ controllers/fighters.controller.js
@@ -2,7 +2,14 @@ import { fighterService } from "../services/fighterService.js"
 
 export const getAllFightersController = (req, res, next) => {
     const fighters = fighterService.getAllFighters()
-    console.log(fighters)
     res.locals.queryResult = fighters
+    next()
+}
+
+
+export const createFighterController = (req, res, next) => {
+    const fighterDetails = req.body
+    const fighter = fighterService.createFighter(fighterDetails)
+    res.locals.queryResult = fighter
     next()
 }
--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,8 +1,47 @@
-import { FIGHTER } from "../models/fighter.js";
+import { FIGHTER_BODY } from "../models/fighter.js";
+import { fighterService } from "../services/fighterService.js";
 
+const fighterFieldsNamesValidator = (obj,shema) => {
+  const fighterKeys = Object.keys(shema);
+  const allKeysExist = fighterKeys.every((key) => obj.hasOwnProperty(key));
+  const noneExtraKeys = Object.keys(obj).every((key) => fighterKeys.includes(key));
+
+  return allKeysExist && noneExtraKeys;
+}
+
+const powerValidator = (power) => power >= 1 && power <= 100 ? true : false
+const defenseValidator = (defense) => defense >= 1 && defense <=10 ? true : false
+const healthValidator = (health) => health >=80 && health <=120 ? true : false
+
+const isFighterInDatabase = (name) => {
+ const isFighterExist = fighterService.search({'name':name})
+ if (isFighterExist) return true
+ return false
+}
 const createFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for FIGHTER entity during creation
-  next();
+  try {
+    req.body.health = 100
+    const fighterBody = req.body
+    console.log(fighterBody)
+    const isFighterExist = isFighterInDatabase(fighterBody.name)
+
+    const isFieldsValid = fighterFieldsNamesValidator(req.body,FIGHTER_BODY)
+    const isPowerValid = powerValidator(fighterBody.power)
+    const isDefenseValid = defenseValidator(fighterBody.defense)
+    const isHealtValid = healthValidator(fighterBody.health)
+
+    if(isFighterExist){
+      throw new Error('Fighter with this name already exist in database')
+    }
+
+    if(!isFieldsValid||!isPowerValid||!isDefenseValid||!isHealtValid){
+      throw new Error('Fighter properties are invalid')
+    }
+    next();
+  } catch (error) {
+    next(error.message)
+  }
+  
 };
 
 const updateFighterValid = (req, res, next) => {
