dbfc4d204d8ebc481775983ade2bc0a288cb8357
Valentyn13 <valentyn13git@gmail.com>
2023-12-16 08:57:52 +0200

    crud fighter controllers created

--- controllers/fighters.controller.js
+++ controllers/fighters.controller.js
@@ -6,10 +6,31 @@ export const getAllFightersController = (req, res, next) => {
     next()
 }
 
-
 export const createFighterController = (req, res, next) => {
     const fighterDetails = req.body
     const fighter = fighterService.createFighter(fighterDetails)
+    res.locals.queryResult = fighter
+    next()
+}
+
+export const updateFighterController = (req, res, next) => {
+    const id = req.params.id
+    const data = req.body
+    const patchedFighter = fighterService.updateFighter(id, data)
+    res.locals.queryResult = patchedFighter
+    next()
+}
+
+export const deletedFighterController = (req, res, next) => {
+    const id = req.params.id
+    const deletedFighter = fighterService.deleteFighter(id)
+    res.locals.queryResult = deletedFighter
+    next()
+}
+
+export const getFighterByIdController = (req, res, next) => {
+    const id = req.params
+    const fighter = fighterService.search(id)
     res.locals.queryResult = fighter
     next()
 }
